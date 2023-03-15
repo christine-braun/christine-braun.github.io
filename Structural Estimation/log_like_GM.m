8605fe9cb3ee730ecae2637914cdba07b254a3ae
christine-braun <cbraun7@gmail.com>
2023-03-15 14:40:05 +0000

    loglikeGM

---
+++ Structural Estimation/log_like_GM.m
@@ -0,0 +1,14 @@
+function [log_like] = log_like_GM()
+%Returns the value of the log likelihood function
+%   inputs: p - parameters, x- data, pi_hat - estimated probailities
+%   output: log_like - negative log likelihood value
+%--------------------------------------------------------------------------
+
+% assign parameters
+mu1 =
+
+% log likelihood function
+log_like = 
+   
+end
+
